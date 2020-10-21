88507e28da60edc15948ad0206690241ad18d29a
Bartosz Kwiecien <info@bkwi.pl>
2020-10-21 19:46:15 +0200

    Resolve homeworld, indicate fetching in progress

--- swexplorer/static/swexplorer/js/collections_list.js
+++ swexplorer/static/swexplorer/js/collections_list.js
@@ -2,14 +2,20 @@ const app = new Vue({
     el: '#app',
 
     data: {
-        collections: []
+        collections: [],
+        processing: false
     },
 
     methods: {
         fetchCollections: function () {
+            if (this.processing) return;
+
+            this.processing = true
             this.$http.post('/api/fetch-collections', {}, reqOptions).then(response => {
+                this.processing = false;
                 console.log("fetch response", response);
             }, response => {
+                this.processing = false;
                 alert(`Something went wrong! Response status: ${response.status}`);
             });
         },
--- swexplorer/utils.py
+++ swexplorer/utils.py
@@ -1,12 +1,24 @@
+from functools import lru_cache
 from django.conf import settings
 
 import requests
 
 
+@lru_cache()
+def resolve_homeworld(url):
+    response = requests.get(url).json()
+    return response['name']
+
+
 def fetch_collection():
     people = []
     addr = f'{settings.SWAPI_HOST}/api/people'
+
     while addr:
         response = requests.get(addr).json()
         addr = response['next']
-        people.extend(response['results'])
+        for item in response['results']:
+            item['homeworld'] = resolve_homeworld(item['homeworld'])
+            people.append(item)
+
+    print(people[0])
